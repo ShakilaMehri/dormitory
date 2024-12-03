95c815a96c2c2aeaf4e9975af0efa691fc93d199
Shakila <shakila.mehri.98@gmail.com>
2024-12-03 12:10:38 +0330

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,35 +2,24 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const vazirFont = localFont({
+  src: "../../public/fonts/Vazirmatn-Regular.woff2",
   weight: "100 900",
 });
 
 export const metadata: Metadata = {
   title: "dormitory",
-  description: "A mdern dormitory management application",
+  description: "A modern dormitory management application",
 };
 
-export default function RootLayout ({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      {children}
-      </body>
+      <body className={vazirFont.className}>{children}</body>
     </html>
   );
 }
